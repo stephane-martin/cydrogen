949b0512e630f447b619213dc85ce713cfbe5059
Stephane MARTIN <stephane.martin1@se.com>
2025-05-20 15:14:53 +0200

    test: add test infrastructure and first test

--- noxfile.py
+++ noxfile.py
@@ -3,21 +3,27 @@ import nox
 nox.options.reuse_existing_virtualenvs = True
 nox.options.error_on_missing_interpreters = True
 
-SUPPORTED_PYTHON_VERSIONS = ("3.12", "3.13")
+PYPROJECT = nox.project.load_toml("pyproject.toml")
+SUPPORTED_PYTHON_VERSIONS = nox.project.python_versions(PYPROJECT)
 
 
 @nox.session(venv_backend="venv", python=SUPPORTED_PYTHON_VERSIONS[-1])
 def develop(session: nox.Session):
-    pyproject = nox.project.load_toml("pyproject.toml")
-    session.install(*nox.project.dependency_groups(pyproject, "develop"))
+    session.install(*nox.project.dependency_groups(PYPROJECT, "develop"))
     session.run("pre-commit", "install")
 
 
 @nox.session(venv_backend="venv", python=SUPPORTED_PYTHON_VERSIONS[-1])
 def lint(session: nox.Session):
-    pyproject = nox.project.load_toml("pyproject.toml")
-    session.install(*nox.project.dependency_groups(pyproject, "develop"))
+    session.install(*nox.project.dependency_groups(PYPROJECT, "develop"))
     session.run("cython-lint", "cydrogen")
     session.run("ruff", "check")
     session.run("ruff", "format", "--check")
     session.run("mypy", "cydrogen")
+
+
+@nox.session(venv_backend="venv", python=SUPPORTED_PYTHON_VERSIONS)
+def test(session: nox.Session):
+    session.install("build >= 0.11.0")
+    session.install(".[test]")
+    session.run("pytest")
