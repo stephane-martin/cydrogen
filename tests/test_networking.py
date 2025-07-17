ef6cbb39373decdef9f3ac7599a10a2e7fb15458
Stephane MARTIN <stephane.martin1@se.com>
2025-07-17 23:27:50 +0200

    test: client/server with key exchange KK and N

--- tests/test_networking.py
+++ tests/test_networking.py
@@ -4,7 +4,15 @@ import time
 import pytest
 from cydrogen import KxPair, Psk
 from cydrogen._networking import MsgQueue
-from cydrogen.networking import RequestResponseHandler, make_kx_xx_client, start_kx_xx_server
+from cydrogen.networking import (
+    RequestResponseHandler,
+    make_kx_kk_client,
+    make_kx_n_client,
+    make_kx_xx_client,
+    start_kx_kk_server,
+    start_kx_n_server,
+    start_kx_xx_server,
+)
 
 
 @pytest.mark.asyncio(loop_scope="module")
@@ -57,9 +65,24 @@ class H(RequestResponseHandler):
 
 CLIENT_PAIR = KxPair("PRd15/pwWvuRunBq5pv8jP1Y10gekV7ld8oH0vcYVC/GWd8Wi87qwB9CV76awCqiicaZAGVhEQvQSgZbPK9g6w==0")
 SERVER_PAIR = KxPair("I4k9+3iOp9BLi5n8HIrYDvoMiJ3MZzkQbE3UU0XWmQIN7g2CCry+J5HqoNe8AzDWwB78nlsRkIwMm5X0VhSZRA==")
+CLIENT_PUBKEY = CLIENT_PAIR.public_key()
+SERVER_PUBKEY = SERVER_PAIR.public_key()
 PSK = Psk("viHijbfh4pyqknE4mvQdD2AqmWB59xrB7yxEv+bN/64=")
 HOST = "127.0.0.1"
 PORT = 8888
+
+MESSAGES = [
+    b"one",
+    b"two",
+    b"three",
+    b"four",
+    b"five",
+    b"six",
+    b"seven",
+    b"eight",
+    b"nine",
+    b"ten",
+]
 
 
 @pytest.mark.asyncio(loop_scope="module")
@@ -68,25 +91,57 @@ async def test_client_server_kx_xx() -> None:
     await server.start_serving()
 
     client = await make_kx_xx_client(HOST, PORT, CLIENT_PAIR, psk=PSK)
-    messages = [
-        b"one",
-        b"two",
-        b"three",
-        b"four",
-        b"five",
-        b"six",
-        b"seven",
-        b"eight",
-        b"nine",
-        b"ten",
-    ]
-    expected_responses = [msg.upper() for msg in messages]
 
+    expected_responses = [msg.upper() for msg in MESSAGES]
     async with client:
-        for msg, expected in zip(messages, expected_responses, strict=True):
+        for msg, expected in zip(MESSAGES, expected_responses, strict=True):
             response = await client.request(msg)
             assert response == expected
 
     # client is closed, server should still be running
+    server.close()
+    await server.wait_closed()
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_client_server_kx_kk() -> None:
+    server: asyncio.Server = await start_kx_kk_server(H, HOST, PORT, SERVER_PAIR, CLIENT_PUBKEY)
+    await server.start_serving()
+
+    client = await make_kx_kk_client(HOST, PORT, CLIENT_PAIR, SERVER_PUBKEY)
+
+    expected_responses = [msg.upper() for msg in MESSAGES]
+    async with client:
+        for msg, expected in zip(MESSAGES, expected_responses, strict=True):
+            response = await client.request(msg)
+            assert response == expected
+
+    # client is closed, server should still be running
+    server.close()
+    await server.wait_closed()
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_client_server_kx_n() -> None:
+    server: asyncio.Server = await start_kx_n_server(H, HOST, PORT, SERVER_PAIR, psk=PSK)
+    await server.start_serving()
+
+    client = await make_kx_n_client(HOST, PORT, SERVER_PUBKEY, psk=PSK)
+    client2 = await make_kx_n_client(HOST, PORT, SERVER_PUBKEY, psk=PSK)
+
+    expected_responses = {msg: msg.upper() for msg in MESSAGES}
+    responses_client1 = {}
+    responses_client2 = {}
+    async with client, client2:  # noqa: SIM117
+        async with asyncio.TaskGroup() as tg:
+            for msg in MESSAGES:
+                responses_client1[msg] = tg.create_task(client.request(msg))
+                responses_client2[msg] = tg.create_task(client2.request(msg))
+    for msg, t in responses_client1.items():
+        assert await t == expected_responses[msg]
+    for msg, t in responses_client2.items():
+        assert await t == expected_responses[msg]
+
+    # clients are closed, server should still be running
     server.close()
     await server.wait_closed()
