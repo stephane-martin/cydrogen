1be6eee8856509486e6b1b9babe5e4212eb3abb0
Stephane MARTIN <stephane.martin1@se.com>
2025-08-04 14:54:19 +0200

    test: add more sync tests with wrong psk/server pubkey

--- tests/test_sync_networking.py
+++ tests/test_sync_networking.py
@@ -16,12 +16,16 @@ from cydrogen.sync_networking import (
 )
 
 CLIENT_PAIR = KxPair("PRd15/pwWvuRunBq5pv8jP1Y10gekV7ld8oH0vcYVC/GWd8Wi87qwB9CV76awCqiicaZAGVhEQvQSgZbPK9g6w==0")
-SERVER_PAIR = KxPair("I4k9+3iOp9BLi5n8HIrYDvoMiJ3MZzkQbE3UU0XWmQIN7g2CCry+J5HqoNe8AzDWwB78nlsRkIwMm5X0VhSZRA==")
-SERVER_PAIR2 = KxPair.gen()
 CLIENT_PUBKEY = CLIENT_PAIR.public_key()
+CLIENT_PAIR_WRONG = KxPair.gen()
+
+SERVER_PAIR = KxPair("I4k9+3iOp9BLi5n8HIrYDvoMiJ3MZzkQbE3UU0XWmQIN7g2CCry+J5HqoNe8AzDWwB78nlsRkIwMm5X0VhSZRA==")
 SERVER_PUBKEY = SERVER_PAIR.public_key()
+SERVER_PAIR_WRONG = KxPair.gen()
+
 PSK = Psk("viHijbfh4pyqknE4mvQdD2AqmWB59xrB7yxEv+bN/64=")
-PSK2 = Psk("wiHijbfh4pyqknE4mvQdD2AqmWB59xrB7yxEv+bN/64=")
+PSK_WRONG = Psk("wiHijbfh4pyqknE4mvQdD2AqmWB59xrB7yxEv+bN/64=")
+
 HOST = "127.0.0.1"
 PORT = 8888
 
@@ -98,7 +102,17 @@ def test_sync_client_sync_server_kx_n_wrong_psk() -> None:
         pass
 
     client = KX_N_TCPClient(HOST, PORT, SERVER_PUBKEY, psk=PSK, connect_retry=10, connect_retry_wait=1)
-    server = KX_N_TCPServer(HOST, PORT, SERVER_PAIR, H, psk=PSK2)
+    server = KX_N_TCPServer(HOST, PORT, SERVER_PAIR, H, psk=PSK_WRONG)
+    with pytest.raises(KeyExchangeException):
+        sync_client_sync_server(client, server)
+
+
+def test_sync_client_sync_server_kx_xx_wrong_psk() -> None:
+    class H(H_Mixin, KX_XX_TCPHandler):
+        pass
+
+    client = KX_XX_TCPClient(HOST, PORT, CLIENT_PAIR, psk=PSK, connect_retry=10, connect_retry_wait=1)
+    server = KX_XX_TCPServer(HOST, PORT, SERVER_PAIR, H, psk=PSK_WRONG)
     with pytest.raises(KeyExchangeException):
         sync_client_sync_server(client, server)
 
@@ -108,6 +122,16 @@ def test_sync_client_sync_server_kx_n_wrong_server_pubkey() -> None:
         pass
 
     client = KX_N_TCPClient(HOST, PORT, SERVER_PUBKEY, psk=PSK, connect_retry=10, connect_retry_wait=1)
-    server = KX_N_TCPServer(HOST, PORT, SERVER_PAIR2, H, psk=PSK)
+    server = KX_N_TCPServer(HOST, PORT, SERVER_PAIR_WRONG, H, psk=PSK)
+    with pytest.raises(KeyExchangeException):
+        sync_client_sync_server(client, server)
+
+
+def test_sync_client_sync_server_kx_kk_wrong_server_pubkey() -> None:
+    class H(H_Mixin, KX_KK_TCPHandler):
+        pass
+
+    client = KX_KK_TCPClient(HOST, PORT, CLIENT_PAIR, SERVER_PUBKEY, connect_retry=10, connect_retry_wait=1)
+    server = KX_KK_TCPServer(HOST, PORT, SERVER_PAIR_WRONG, H, CLIENT_PUBKEY)
     with pytest.raises(KeyExchangeException):
         sync_client_sync_server(client, server)
